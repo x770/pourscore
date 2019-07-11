016cf62f6f91d1fe5eee30eaa02346670e45603f
Travis Henson <travisghenson@gmail.com>
2019-07-11 19:58:27 -0400

    Attempting routing fix for the 100th time

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Nav from './components/Nav';
 import Welcome from './pages/Welcome';
